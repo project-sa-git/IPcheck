1b95001f79e9a00f2cc74f50dbdb1f8d5f871849
mac2nd <trybook1901@gmail.com>
2020-08-16 00:51:32 +0900

    valid_func&add_txt

--- valida_ip_check.rb
+++ valida_ip_check.rb
@@ -1,1 +1,17 @@
-puts "hello world!"
+require 'ipaddr'
+
+# 判定メソッド
+def valid_ip?(str)
+  !!IPAddr.new(str) rescue false
+end
+
+# ファイルを読み込んで空配列に格納
+ary = []
+File.foreach("samp_ip.txt"){|line|
+  ary << line.chomp
+}
+
+# IPアドレスの判定
+for var in ary do
+  p valid_ip?(var)
+end
