b3996cc71f66b9cc1855e0b5404554f171c9838c
mac2nd <trybook1901@gmail.com>
2020-08-16 14:20:49 +0900

    add-Type&change-txt

--- valida_ip_check.rb
+++ valida_ip_check.rb
@@ -1,17 +1,42 @@
 require 'ipaddr'
 
-# 判定メソッド
+# バリデーション判定メソッド
 def valid_ip?(str)
   !!IPAddr.new(str) rescue false
 end
 
-# ファイルを読み込んで空配列に格納
-ary = []
+# プライベートネットワーク範囲の判定メソッド
+def private_network?(ipaddr)
+  [
+    IPAddr.new('10.0.0.0/8'),
+    IPAddr.new('172.16.0.0/12'),
+    IPAddr.new('192.168.0.0/16')
+  ].any? {|i| i.include? ipaddr }
+end
+
+
+# リストファイルを読み込んで空配列に格納
+array = []
 File.foreach("samp_ip.txt"){|line|
-  ary << line.chomp
+  array << line.chomp
 }
 
-# IPアドレスの判定
-for var in ary do
-  p valid_ip?(var)
+# IPアドレスの判定（メイン処理）
+puts "IPアドレスの判定一覧"
+for var in array do
+  puts "ーーーー"
+  puts "Input IP : #{var}"
+  if valid_ip?(var)
+    puts "Validate : OK"
+    if private_network?(var)
+      puts "Type     : Private"
+    else
+      puts "Type     : Global"
+    end
+  else
+    puts "Validate : Error"  
+    puts "Type     : -"
+  end
+  puts "ーーーー"
+  puts ""
 end
